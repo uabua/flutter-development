269adc04f2e3959150b16029ff8064894ccefb0d
uabua <mariami.chakhvadze.1@btu.edu.ge>
2021-04-30 22:33:56 +0400

    Update routes

--- midterms/midterm_01/lib/main.dart
+++ midterms/midterm_01/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:midterm_01/screens/add_car.dart';
+import 'package:midterm_01/screens/car_details.dart';
+import 'package:midterm_01/screens/edit_car.dart';
 import 'package:midterm_01/screens/home.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Home(),
-    // routes: {
-
-    // },
+    routes: {
+      CarDetails.routeName: (context) => CarDetails(),
+      EditCar.routeName: (context) => EditCar(),
+      AddCar.routeName: (context) => AddCar(),
+    },
   ));
 }
