0f128e4a5e5248196ba30e84c56aeda3285b46da
uabua <mariami.chakhvadze.1@btu.edu.ge>
2021-04-30 22:31:43 +0400

    Update Home Screen

--- midterms/midterm_01/lib/screens/home.dart
+++ midterms/midterm_01/lib/screens/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:midterm_01/data/cars.dart';
+import 'package:midterm_01/screens/add_car.dart';
+import 'package:midterm_01/screens/car_details.dart';
+import 'package:midterm_01/screens/edit_car.dart';
 import 'package:midterm_01/widgets/car_item.dart';
 
 class Home extends StatefulWidget {
@@ -14,15 +17,37 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Cars"),
         centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => Navigator.pushNamed(
+              context,
+              AddCar.routeName,
+            ).then((_) => setState(() {})),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: cars.length,
         itemBuilder: (context, index) {
           var car = cars[index];
-          return CarItem(
-            imageUrl: car.imageUrl,
-            name: "${car.brand} ${car.model}",
-            year: car.year,
+
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(
+              context,
+              CarDetails.routeName,
+              arguments: index,
+            ).then((_) => setState(() {})),
+            onLongPress: () => Navigator.pushNamed(
+              context,
+              EditCar.routeName,
+              arguments: index,
+            ).then((_) => setState(() {})),
+            child: CarItem(
+              imageUrl: car.imageUrl,
+              name: "${car.brand} ${car.model}",
+              year: car.year,
+            ),
           );
         },
       ),
