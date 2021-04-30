3e880154f33fd83af5e04d27f18491aa9bf9c798
uabua <mariami.chakhvadze.1@btu.edu.ge>
2021-04-30 19:55:22 +0400

    Update Car model

--- midterms/midterm_01/lib/data/models/car.dart
+++ midterms/midterm_01/lib/data/models/car.dart
@@ -1,19 +1,17 @@
-import 'package:flutter/foundation.dart';
-
 class Car {
   final int id;
   final String brand;
   final String model;
-  final String imageUrl;
   final int year;
   final String description;
+  final String imageUrl;
 
   Car({
-    @required this.id,
-    @required this.brand,
-    @required this.model,
-    @required this.year,
-    @required this.description,
-    @required this.imageUrl,
+    required this.id,
+    required this.brand,
+    required this.model,
+    required this.year,
+    required this.description,
+    required this.imageUrl,
   });
 }
