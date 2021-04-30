29a8fcbf29705b7fa98a2c1a92de80d85539ee81
uabua <mariami.chakhvadze.1@btu.edu.ge>
2021-04-30 21:42:32 +0400

    Add Car item

---
+++ midterms/midterm_01/lib/widgets/car_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CarItem extends StatelessWidget {
+  final String imageUrl;
+  final String name;
+  final int year;
+
+  CarItem({
+    required this.imageUrl,
+    required this.name,
+    required this.year,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Card(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: FadeInImage(
+                placeholder:
+                    AssetImage("assets/placeholders/car_placeholder.webp"),
+                image: NetworkImage(imageUrl),
+                width: 300,
+                height: 200,
+              ),
+            ),
+            Text(
+              name,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Text(
+              "$year",
+              style: Theme.of(context).textTheme.caption,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
