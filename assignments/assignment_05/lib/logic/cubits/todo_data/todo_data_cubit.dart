5631295789db8ca0d9c2cdb89503500449fdfcda
uabua <mariami.chakhvadze.1@btu.edu.ge>
2021-05-24 11:35:38 +0400

    Update logic

--- assignments/assignment_05/lib/logic/cubits/todo_data/todo_data_cubit.dart
+++ assignments/assignment_05/lib/logic/cubits/todo_data/todo_data_cubit.dart
@@ -10,11 +10,58 @@ class TodoDataCubit extends Cubit<TodoDataState> {
 
   final todoRepository = TodoRepository();
 
-  Future<void> getTodoData(String userID) async {
+  Future<void> addTodoData(Map<String, dynamic> todoData) async {
+    final todos = (state as TodoDataLoaded).todos;
+
     emit(TodoDataInitial());
 
     try {
-      var list = await todoRepository.fetchTodoByUserID(userID);
+      await todoRepository.addTodoData(todoData);
+
+      final todo = Todo(
+        id: todos.last.id! + 1,
+        userId: todoData['userId'],
+        title: todoData['title'],
+        completed: false,
+      );
+
+      todos.insert(0, todo);
+
+      emit(TodoDataLoaded(
+        todos: todos,
+      ));
+    } catch (e) {
+      emit(TodoDataError(
+        message: e.toString(),
+      ));
+    }
+  }
+
+  Future<void> deleteTodoData(int todoId) async {
+    final todos = (state as TodoDataLoaded).todos;
+
+    emit(TodoDataInitial());
+
+    try {
+      await todoRepository.deleteTodoData(todoId);
+
+      todos.removeWhere((todo) => todo.id == todoId);
+
+      emit(TodoDataLoaded(
+        todos: todos,
+      ));
+    } catch (e) {
+      emit(TodoDataError(
+        message: e.toString(),
+      ));
+    }
+  }
+
+  Future<void> getTodoData(String userId) async {
+    emit(TodoDataInitial());
+
+    try {
+      var list = await todoRepository.fetchTodoByUserID(userId);
       emit(TodoDataLoaded(
         todos: list!,
       ));
