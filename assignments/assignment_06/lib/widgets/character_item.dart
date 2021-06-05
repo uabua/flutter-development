d24cbe63484da319d01382b673d60fc8230734bd
uabua <mariami.chakhvadze.1@btu.edu.ge>
2021-06-05 20:15:27 +0400

    Update character item widget

--- assignments/assignment_06/lib/widgets/character_item.dart
+++ assignments/assignment_06/lib/widgets/character_item.dart
@@ -4,22 +4,12 @@ class CharacterItem extends StatelessWidget {
   final String image;
   final String name;
   final String status;
-  final String species;
-  final String type;
-  final String gender;
-  final String origin;
-  final String location;
 
   const CharacterItem({
     Key? key,
     required this.image,
     required this.name,
     required this.status,
-    required this.species,
-    required this.type,
-    required this.gender,
-    required this.origin,
-    required this.location,
   }) : super(key: key);
 
   @override
@@ -33,7 +23,7 @@ class CharacterItem extends StatelessWidget {
               borderRadius: BorderRadius.circular(150.0),
               child: FadeInImage(
                 placeholder: const AssetImage(
-                  "assets/placeholders/placeholder.png",
+                  'assets/placeholders/placeholder.png',
                 ),
                 image: NetworkImage(
                   image,
@@ -61,70 +51,6 @@ class CharacterItem extends StatelessWidget {
             ),
           ],
         ),
-        // child: Row(
-        //   children: [
-        //     Expanded(
-        //       flex: 1,
-        //       child: ClipRRect(
-        //         borderRadius: BorderRadius.circular(150.0),
-        //         child: FadeInImage(
-        //           placeholder: const AssetImage(
-        //             "assets/placeholders/placeholder.png",
-        //           ),
-        //           image: NetworkImage(
-        //             image,
-        //           ),
-        //         ),
-        //       ),
-        //     ),
-        //     Expanded(
-        //       flex: 2,
-        //       child: Column(
-        //         children: [
-        //           Row(
-        //             mainAxisAlignment: MainAxisAlignment.end,
-        //             children: [
-        //               _getStatusIcon(status),
-        //               Text(
-        //                 status,
-        //                 style: Theme.of(context).textTheme.bodyText1,
-        //               ),
-        //             ],
-        //           ),
-        //           Text(
-        //             name,
-        //             style: const TextStyle(
-        //               fontWeight: FontWeight.bold,
-        //               fontSize: 20,
-        //             ),
-        //             textAlign: TextAlign.center,
-        //           ),
-
-        //           // Text(
-        //           //   'aa ${species == '' ? 'N/A' : species}',
-        //           //   style: Theme.of(context).textTheme.caption,
-        //           // ),
-        //           // Text(
-        //           //   'type - ${type == '' ? 'N/A' : type}',
-        //           //   style: Theme.of(context).textTheme.caption,
-        //           // ),
-        //           // Text(
-        //           //   'gender - ${gender == '' ? 'N/A' : gender}',
-        //           //   style: Theme.of(context).textTheme.caption,
-        //           // ),
-        //           // Text(
-        //           //   'origin - ${origin == '' ? 'N/A' : origin}',
-        //           //   style: Theme.of(context).textTheme.caption,
-        //           // ),
-        //           // Text(
-        //           //   'last known location - ${location == '' ? 'N/A' : location}',
-        //           //   style: Theme.of(context).textTheme.caption,
-        //           // ),
-        //         ],
-        //       ),
-        //     ),
-        //   ],
-        // ),
       ),
     );
   }
