63b1b7a8cfe9c68ba23ec13989bcdf97bfbf4542
uabua <mariami.chakhvadze.1@btu.edu.ge>
2021-06-26 21:59:37 +0400

    Update colors.dart

--- final-project/final_project/lib/utils/colors.dart
+++ final-project/final_project/lib/utils/colors.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
 const backgroundColor = Color(0xFFFFFFFF);
-const loginScreenLight = Color(0xFFa8e3e8);
-const loginScreenTextDark = Color(0xFF030303);
-
-const primaryColor = Color(0xFF95d3c4);
-const primaryColorAccent = Color(0xFFcbefef);
-const primaryColorDark = Color(0xFF267b7b);
-const secondaryColorBlack = Color(0xFF000000);
+const customLightGreen = Color(0xFFa8e3e8);
+const textDark = Color(0xFF030303);
+const customDarkGreen = Color(0xFF267b7b);
+const bottomSheetBackground = Color(0xFFcbefef);
